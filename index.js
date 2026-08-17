a3c68857f0bd16d7003ddcabe81be06cb521ae2e
Jak1ro <shevchenko0618@gmail.com>
2026-08-17 22:41:44 +0300

    2

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// const lightField = document.querySelector('.new-head-dropdown');
+// const grayField = document.querySelector('.new-gray-field');
+// const mobileMenu = document.querySelector('.new-mob-menu');
+// const body = document.getElementById('body');
+// const btn = document.querySelector('.new-header-burger');
+
+
+// function openMobile(e) {
+//   e.preventDefault();
+//   lightField.classList.add('open');
+//   mobileMenu.classList.add('slide');
+//   body.classList.add('no-scroll');
+//   grayField.addEventListener('click', closeMobile);
+// }
+
+// function closeMobile(e) {
+//   e.preventDefault();
+//   lightField.classList.remove('open');
+//   mobileMenu.classList.remove('slide');
+//   body.classList.remove('no-scroll');
+// }
+
+
 const items = document.querySelectorAll('#myList li');
   let currentIndex = 0;
 
@@ -6,3 +29,6 @@ const items = document.querySelectorAll('#myList li');
     currentIndex = (currentIndex + 1) % items.length;
     items[currentIndex].classList.add('active');
   }, 4000);
+
+
+  // btn.addEventListener('click', openMobile);
