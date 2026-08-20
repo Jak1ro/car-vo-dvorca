6c6d9398e1ec5a738771067cf707d84fa3927fd5
Jak1ro <shevchenko0618@gmail.com>
2026-08-21 02:28:40 +0300

    отправляю

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const grayField = document.querySelector('.new-gray-field');
 const mobileMenu = document.querySelector('.new-mob-menu');
 const body = document.getElementById('body');
 const btn = document.querySelector('.new-header-burger');
+const xxx = document.querySelector('.new-mob-btn-close');
+const buttons = document.querySelectorAll('.tab-btn');
+const panels = document.querySelectorAll('.tab-panel');
+const headModal = document.querySelector('.header-modal');
+const topMenuClose = document.querySelector('.top-menu-close');
+const headerBurger = document.getElementById('header-burger')
 
+function openModal(e) {
+  e.preventDefault();
+  headModal.classList.add('active')
+  topMenuClose.addEventListener('click', closeModal)
+  headModal.addEventListener('click', closeModal)
+}
+
+function closeModal(e) {
+  e.preventDefault();
+  headModal.classList.remove('active')
+}
+
+headerBurger.addEventListener('click', openModal)
 
 function openMobile(e) {
   e.preventDefault();
@@ -11,6 +30,7 @@ function openMobile(e) {
   mobileMenu.classList.add('slide');
   body.classList.add('no-scroll');
   grayField.addEventListener('click', closeMobile);
+  xxx.addEventListener('click', closeMobile);
 }
 
 function closeMobile(e) {
@@ -32,3 +52,25 @@ const items = document.querySelectorAll('#myList li');
 
 
   btn.addEventListener('click', openMobile);
+
+  
+
+function switchTab(event) {
+  const targetBtn = event.currentTarget;
+  const targetPanelId = targetBtn.getAttribute('data-tab');
+  // Очищаем активные классы у кнопок и панелей
+  buttons.forEach(btn => {
+    btn.classList.remove('active');
+    btn.setAttribute('aria-selected', 'false');
+  });
+  panels.forEach(panel => panel.classList.remove('active'));
+  // Добавляем активный класс текущей кнопке и панели
+  targetBtn.classList.add('active');
+  targetBtn.setAttribute('aria-selected', 'true');
+  document.getElementById(targetPanelId).classList.add('active');
+}
+// Вешаем обработчики на наведение (mouseenter) и фокус (focus)
+buttons.forEach(button => {
+  button.addEventListener('mouseenter', switchTab);
+  button.addEventListener('focus', switchTab);
+});
